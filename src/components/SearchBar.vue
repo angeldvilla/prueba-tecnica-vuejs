896ad636041934cfe7390c85c5472c92974e3f65
angeldvilla <escarlata.2003@gmail.com>
2024-09-16 16:31:04 -0500

    Componente para busqueda de país especifico

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from "vue";
+
+const emit = defineEmits(["search"]);
+
+const searchTerm = ref("");
+
+// Función para cuando el usuario escribe en el campo de búsqueda
+const search = () => {
+  emit("search", searchTerm.value);
+};
+</script>
+
+<template>
+  <input
+    type="search"
+    placeholder="Busca un país..."
+    class="rounded-full p-2 px-4 border-2 focus:outline-none focus:border-blue-500"
+    v-model="searchTerm"
+    @keyup.enter="search"
+  />
+  <button
+      class="bg-blue-500 text-white rounded-full px-4 py-2"
+      @click="search"
+    >
+      Buscar
+    </button>
+</template>
